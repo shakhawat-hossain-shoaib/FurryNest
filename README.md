25db7ea0840e1bc4a544b5e300b070bae5f23b55
shakhawat-hossain-shoaib <shakhawat.cse.20230104081@aust.edu>
2025-08-23 21:35:52 +0600

    Updated

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Choose adoption over buying. Give a loving home to cats and dogs who need it most. 
 Every adoption saves a life and creates space for another animal in need.
 
+
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { SiInstagram } from "react-icons/si";
+import { SiFacebook } from "react-icons/si";
+import { SiTiktok } from "react-icons/si";
 
 const Footer = () => (
   <footer>
     <div className="social-icons">
-      <a href="#"><span role="img" aria-label="twitter">🐦</span></a>
-      <a href="#"><span role="img" aria-label="facebook">📘</span></a>
-      <a href="#"><span role="img" aria-label="instagram">📸</span></a>
-      <a href="#"><span role="img" aria-label="tiktok">🎵</span></a>
+      <a href="."><span aria-label="facebook"><SiFacebook /></span></a>
+      <a href="."><span aria-label="instagram"><SiInstagram /></span></a>
+      <a href="."><span aria-label="tiktok"><SiTiktok /></span></a>
     </div>
     <p>| Designed by Shoaib, Hasan, Rifat |</p>
   </footer>
